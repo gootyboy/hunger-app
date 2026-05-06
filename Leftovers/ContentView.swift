e354baad6b4d0cc0e2909ba8496d3f482e588f0e
Gooty <gootyboy@icloud.com>
2026-05-05 21:31:40 -0400

    Initial Commit

---
+++ Leftovers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Leftovers
+//
+//  Created by Gautam Pulugurta on 5/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Leftovers/LeftoversApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeftoversApp.swift
+//  Leftovers
+//
+//  Created by Gautam Pulugurta on 5/5/26.
+//
+
+import SwiftUI
+
+@main
+struct LeftoversApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
